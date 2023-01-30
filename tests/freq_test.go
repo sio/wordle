0e3d642cd6baf7a78fc99ead3d542fdafd93ea9d
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-01-30 11:36:23 +0300

    Validate scoring logic for repeated words and letters

--- tests/freq_test.go
+++ tests/freq_test.go
@@ -21,4 +21,31 @@ func TestFreq(t *testing.T) {
 	if delta > 1e-6 || delta < -1e-6 {
 		t.Errorf("sum of character probabilities is not 1.0: %v", total)
 	}
+
+	inputs := []string{
+		"hello",
+		"алоха",
+		"house",
+		"домик",
+	}
+	var w wordle.Word
+	for _, input := range inputs {
+		w.Parse(input)
+		single := chars.Score(w)
+		double := chars.Score(w, w)
+		triple := chars.Score(w, w, w)
+		if single != double {
+			t.Errorf("single word score (%v) does not match double word score (%v) for %q", single, double, w)
+		}
+		if single != triple {
+			t.Errorf("single word score (%v) does not match triple word score (%v) for %q", single, triple, w)
+		}
+	}
+
+	w.Parse("яяяяя")
+	wordScore := chars.Score(w)
+	charScore := chars['я']
+	if wordScore != charScore {
+		t.Errorf("repeated word score (%v) does not match single char score (%v)", wordScore, charScore)
+	}
 }
